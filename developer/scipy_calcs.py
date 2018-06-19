29d729c629a913a92d6efbd8981f23d3ab9e3c65
Lucas Kotres <lucaskotres@gmail.com>
2018-06-18 22:56:42 -0300

    More Examples

---
+++ developer/scipy_calcs.py
@@ -0,0 +1,10 @@
+
+import epmprocessor as epr
+import epmwebapi as epm
+
+import scipy
+#examples with linear algebra and integrate
+
+@epr.applicationMethod('CurveArea')
+def curve_area(session, epmdataobject):
+

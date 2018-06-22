ffa1c79f12f0d0f2cd51bf2434f40274acbeb335
kotres <lucaskotres@gmail.com>
2018-06-22 15:53:24 -0300

    just refactor

--- basic/rw_resources.py
+++ basic/rw_resources.py
@@ -1,6 +1,16 @@
 ***REMOVED***
 ***REMOVED***
 
+@epr.applicationMethod('GetPortalResource')
+def get_portal_resource(session, filename, connection):
+    '''We can get resources from EPM WebServer, first create the connection in Workbench/Settings/Connections'''
+    #TODO: verificar como usar um parametro do tipo epmconnection
+
+    print(session.connections)
+
+    epResourceManager = epmConn.getProcessorResourcesManager()
+    resource = epResourceManager.getResource(fileModel)
+
 @epr.applicationMethod('GetProcessorResource')
 def get_processor_resource(session, filename, connection):
     '''We can get resources from EPM WebServer, first create the connection in Workbench/Settings/Connections'''

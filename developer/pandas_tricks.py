f4d5d6883365c0547dce64174b50618eecf7a800
kotres <lucaskotres@gmail.com>
2018-06-22 15:53:24 -0300

    just refactor

--- developer/pandas_tricks.py
+++ developer/pandas_tricks.py
@@ -2,72 +2,85 @@
 import epmprocessor as epr
 import epmwebapi as epm
 
+import numpy as np
 import datetime
 import pandas as pd
 from pandas.tseries.offsets import *
 
 #pandas is the most powerfull Python library to work with tables
 
-@epr.applicationMethod('MondaysAvg')
-def mondays_avg(session, epmdataobject, starttime, endtime):
-    '''Calculates the average of every Monday of the last month'''
+@epr.applicationMethod('MondaysDescribe')
+def mondays_describe(session, epmdataobject, starttime, endtime):
+    '''Shows the statistical describe of every Monday in period'''
 
-
-
-    #endtime = datetime.datetime.now()
-    #starttime = endtime - datetime.timedelta(days=30)
 
     try:
-        queryperiod = epm.QueryPeriod(starttime,endtime)
+        queryperiod = epm.QueryPeriod(starttime, endtime)
         processInterval = datetime.timedelta(seconds=300)
         aggregationdetails = epm.AggregateDetails(processInterval, epm.AggregateType.Interpolative)
-        data = epmdataobject.historyReadAggregate(aggregationdetails,queryperiod)
+        data = epmdataobject.historyReadAggregate(aggregationdetails, queryperiod)
 
     except:
         raise Exception('Error in read aggregation')
 
-    print(data)
-    #EPM return ndarray, we need to transform in pandas dataframe
-
+    # EPM return ndarray, we need to transform in pandas dataframe
     df = pd.DataFrame(
-        {'Value':data['Value'],
-         'Timestamp':data['Timestamp']}
+        {'Value': data['Value'].tolist(),
+         'Timestamp': data['Timestamp'].tolist()}
     )
-    print(df)
+    #creating a new column with the weekday name
+    df['day_of_week'] = df['Timestamp'].dt.weekday_name
 
-    #TODO: more elegant method
-    #mondays = df.loc(df['Timestamp'].dt.dayofweek == 0)
-    #mondays = [item for item in df if df['Timestamp'].dt.dayofweek == 0]  # monday=0, sunday=6
-    mondays_idx = [item for item in df['Timestamp'].dt.dayofweek == 0]
-
-    for item in len(mondays_idx):
-        if item == True:
+    #fitering by Monday
+    filtered_data = df[df.day_of_week == 'Monday']
 
 
+    print(filtered_data.describe())
 
-
-
-
-    print(mondays)
-
-    return epr.ScopeResult(True)
 
 @epr.applicationMethod('DaytimeWorkConsumption')
 def daytime_work_comsumption(session, epmdataobject, starttime, endtime):
     '''calculates the energy consumption in working day from Monday to Friday'''
+    try:
+        queryperiod = epm.QueryPeriod(starttime, endtime)
+        processInterval = datetime.timedelta(seconds=300)
+        aggregationdetails = epm.AggregateDetails(processInterval, epm.AggregateType.Interpolative)
+        data = epmdataobject.historyReadAggregate(aggregationdetails, queryperiod)
+
+    except:
+        raise Exception('Error in read aggregation')
+
+    # EPM return ndarray, we need to transform in pandas dataframe
+    df = pd.DataFrame(
+        {'Value': data['Value'].tolist(),
+         'Timestamp': data['Timestamp'].tolist()}
+    )
 
 
-    data = df.asfreq(BDay())
-    data = df.between_time('08:00', '18:00')
+    newdf = df.asfreq(BDay())
+    bdh = newdf.between_time('08:00', '18:00')
 
-    print(data)
+    print(bdh)
 
-    return epr.sessionResult(True)
+    return epr.ScopeResult(True)
 
 @epr.applicationMethod('TimeBelow')
 def timebelow(session, epmdataobject, lowerlimit, starttime, endtime):
-
     '''calculates the total time that the variable was below a limit value'''
+    try:
+        queryperiod = epm.QueryPeriod(starttime, endtime)
+        processInterval = datetime.timedelta(seconds=300)
+        aggregationdetails = epm.AggregateDetails(processInterval, epm.AggregateType.Interpolative)
+        data = epmdataobject.historyReadAggregate(aggregationdetails, queryperiod)
+
+    except:
+        raise Exception('Error in read aggregation')
+
+    # EPM return ndarray, we need to transform in pandas dataframe
+    df = pd.DataFrame(
+        {'Value': data['Value'].tolist(),
+         'Timestamp': data['Timestamp'].tolist()}
+    )
 
     df = df.reset_index()
 

3c3b809b0260cdedd8ccd9cfefb0aca325833107
kotres <lucaskotres@gmail.com>
2018-06-22 17:08:25 -0300

    pre_processing for machine learning experiments

---
+++ advanced/pre_processing.py
@@ -0,0 +1,14 @@
+
+import epmprocessor as epr
+import epmwebapi as epm
+
+
+@epr.applicationMethod('RemoveOutliers')
+def remove_outliers(epmdataobject):
+
+    pass
+@epr.applicationMethod('OneHotEncoding')
+def one_hot_encoding(epmdataobject):
+
+    pass
+

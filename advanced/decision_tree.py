762c8ba041d0a1f878f8de55379cab0bd9cb570b
Lucas Kotres <lucaskotres@gmail.com>
2018-08-08 13:12:38 -0300

    decision tree

--- advanced/decision_tree.py
+++ advanced/decision_tree.py
@@ -1,41 +1,44 @@
-'''
-import epmprocessor as epr
-import epmwebapi as epm
 import numpy as np
 import matplotlib.pyplot as plt
+import epmprocessor as epr
+import epmwebapi as epm
 import datetime
 
-from sklearn.datasets import load_iris
+import mimetypes
+import io
+from collections import OrderedDict
+
 from sklearn.tree import DecisionTreeRegressor
+import pandas as pd
 
 
 @epr.applicationMethod('PredictVariable')
-def predict_variable(epmdataobject1,epmdataobject2, starttime, endtime, connection, pathname):
-
-    queryperiod = epm.QueryPeriod(starttime,endtime)
-    processInterval = datetime.timedelta(seconds=60)
-    aggregationdetails = epm.AggregateDetails(processInterval, epm.AggregateType.Interpolative)
-
-    data1 = epmdataobject1.historyReadAggregate(aggregationdetails,queryperiod)
-    data2 = epmdataobject2.historyReadAggregate(aggregationdetails,queryperiod)
-
-    # Load data
-
-    X = np.empty(1000)
-
-    for i in range(1000):
-
-        X.append(data1['Value'][i],data2['Value'][i])
+def predict_variable(session, epmdataobject1, epmdataobject2, starttime, endtime, connection, pathname):
 
 
-    #X = np.array([data1['Value'][0:1000],data2['Value'][0:1000]])
-    y = data2['Value'][0:1000]
+    try:
 
+        queryperiod = epm.QueryPeriod(starttime, endtime)
+        processInterval = datetime.timedelta(seconds=600)
+        aggregationdetails = epm.AggregateDetails(processInterval, epm.AggregateType.Interpolative)
 
-    X_test = np.empty([data1['Value'][1500],data2['Value'][1500]])
+        data1 = epmdataobject1.historyReadAggregate(aggregationdetails, queryperiod)
+        data2 = epmdataobject2.historyReadAggregate(aggregationdetails, queryperiod)
+    except Exception:
+        print('Erro ao consultar dataobjects')
 
+    df1 = pd.DataFrame(
+        {'Value': data1['Value'].tolist()})
 
-    print(X_test)
+    df2 = pd.DataFrame(
+        {'Value': data2['Value'].tolist()})
+
+    y = df1.iloc[:, 0:1].values
+
+    X = df2.iloc[:, 0:1].values
+
+    X_test = np.array([0, 3, 6, 9, 12, 15, 18])
+    X_test = X_test.reshape(-1, 1)
 
     # Fit regression model
     regr_1 = DecisionTreeRegressor(max_depth=2)
@@ -44,80 +47,36 @@ def predict_variable(epmdataobject1,epmdataobject2, starttime, endtime, connecti
     regr_2.fit(X, y)
 
     # Predict
-    
+
     y_1 = regr_1.predict(X_test)
+    print(y_1)
+    print(X_test)
     y_2 = regr_2.predict(X_test)
+
+    print(y_2)
+    print(X_test)
 
     # Plot the results
     plt.figure()
-    plt.scatter(X, y, s=20, edgecolor="black",
-                c="darkorange", label="data")
-    plt.plot(X_test, y_1, color="cornflowerblue",
-             label="max_depth=2", linewidth=2)
-    plt.plot(X_test, y_2, color="yellowgreen", label="max_depth=5", linewidth=2)
+    plt.scatter(X, y, color='red', label="data")
+    plt.plot(X_test, regr_1.predict(X_test), color="blue", label="max_depth=2", linewidth=2)
+    plt.plot(X_test, regr_2.predict(X_test), color="yellow", label="max_depth=5", linewidth=2)
     plt.xlabel("data")
     plt.ylabel("target")
     plt.title("Decision Tree Regression")
     plt.legend()
-    bufBoxplot = io.BytesIO()
-    plt.savefig(bufBoxplot, format='png')
-    bufBoxplot.seek(0)
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
 
-
-    print(session.connections)
     epmConn = getFirstItemFromODict(session.connections)
     epResourceManager = epmConn.getPortalResourcesManager()
     imgFolder = epResourceManager.getResource(pathname)
 
-    resource = imgFolder.upload('scatter.png', bufBoxplot, 'Scatterplot gerado pelo Processor',mimetypes.types_map['.png'], overrideFile=True)
-
-'''
-#################################
-import csv
-import numpy as np
-import matplotlib.pyplot as plt
-
-import pandas as pd
-
-dataset = pd.read_csv('c:\epmprocessorexamples\wind_data.csv',delimiter=';')
+    resource = imgFolder.upload('predict_Decision_tree_regression.png', buf, 'Scatterplot gerado pelo Processor',
+                                mimetypes.types_map['.png'],
+                                overrideFile=True)
 
 
-y = dataset.iloc[:,0:1].values
-
-X = dataset.iloc[:, 1:2].values
-
-X_test = np.array([0,3,6, 9,12,15,18])
-X_test = X_test.reshape(-1,1)
-
-import datetime
-
-from sklearn.datasets import load_iris
-from sklearn.tree import DecisionTreeRegressor
-
-
-# Fit regression model
-regr_1 = DecisionTreeRegressor(max_depth=2)
-regr_2 = DecisionTreeRegressor(max_depth=5)
-regr_1.fit(X, y)
-regr_2.fit(X, y)
-
-# Predict
-
-y_1 = regr_1.predict(X_test)
-print(y_1)
-print(X_test)
-y_2 = regr_2.predict(X_test)
-
-print(y_2)
-print(X_test)
-
-# Plot the results
-plt.figure()
-plt.scatter(X, y, color='red', label="data")
-plt.plot(X_test, regr_1.predict(X_test), color="blue", label="max_depth=2", linewidth=2)
-plt.plot(X_test, regr_2.predict(X_test), color="yellow", label="max_depth=5", linewidth=2)
-plt.xlabel("data")
-plt.ylabel("target")
-plt.title("Decision Tree Regression")
-plt.legend()
-plt.show()
+def getFirstItemFromODict(oDict):
+    return next(iter(oDict.values())) if isinstance(oDict, OrderedDict) or isinstance(oDict, dict) else None
---
+++ advanced/decision_tree_test.py
@@ -0,0 +1,104 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import epmprocessor as epr
+import epmwebapi as epm
+import datetime
+import mimetypes
+import io
+from collections import OrderedDict
+
+from sklearn.tree import DecisionTreeRegressor
+import pandas as pd
+
+def getFirstItemFromODict(oDict):
+    return next(iter(oDict.values())) if isinstance(oDict, OrderedDict) or isinstance(oDict, dict) else None
+
+try:
+    connection = epm.EpmConnection('http://dili:44333', 'http://dili:44332', 'sa', 'Epm2017')
+
+except Exception:
+    logger.error("can't connect to epm server")
+    exit(1)
+
+try:
+    dataobject1 = connection.getDataObjects('SP01_PowerAvg')
+
+except Exception:
+    logger.error("can't found {} in the epm Server".format(bvname))
+    exit(1)
+
+try:
+    dataobject2 = connection.getDataObjects('SP01_WindSpeedAvg')
+
+except Exception:
+    logger.error("can't found {} in the epm Server".format(bvname))
+    exit(1)
+
+starttime = datetime.datetime(2014, 3, 1, 1, 00, 00)
+endtime = datetime.datetime(2014, 3, 30, 1, 00, 00)
+
+
+
+queryperiod = epm.QueryPeriod(starttime,endtime)
+processInterval = datetime.timedelta(seconds=600)
+aggregationdetails = epm.AggregateDetails(processInterval, epm.AggregateType.Interpolative)
+
+data1 = dataobject1['SP01_PowerAvg'].historyReadAggregate(aggregationdetails,queryperiod)
+data2 = dataobject2['SP01_WindSpeedAvg'].historyReadAggregate(aggregationdetails,queryperiod)
+
+df1 = pd.DataFrame(
+    {'Value': data1['Value'].tolist()})
+
+df2 = pd.DataFrame(
+    {'Value': data2['Value'].tolist()})
+
+
+y = df1.iloc[:,0:1].values
+
+X = df2.iloc[:,0:1].values
+
+
+#y.reshape(1,-1)
+#X.reshape(-1,1)
+
+
+#y = dataset.iloc[:,0:1].values
+
+#X = dataset.iloc[:, 1:2].values
+
+X_test = np.array([0, 3, 6, 9, 12, 15, 18])
+X_test = X_test.reshape(-1, 1)
+
+
+
+# Fit regression model
+regr_1 = DecisionTreeRegressor(max_depth=2)
+regr_2 = DecisionTreeRegressor(max_depth=5)
+regr_1.fit(X, y)
+regr_2.fit(X, y)
+
+# Predict
+
+y_1 = regr_1.predict(X_test)
+print(y_1)
+print(X_test)
+y_2 = regr_2.predict(X_test)
+
+print(y_2)
+print(X_test)
+
+# Plot the results
+plt.figure()
+plt.scatter(X, y, color='red', label="data")
+plt.plot(X_test, regr_1.predict(X_test), color="blue", label="max_depth=2", linewidth=2)
+plt.plot(X_test, regr_2.predict(X_test), color="yellow", label="max_depth=5", linewidth=2)
+plt.xlabel("data")
+plt.ylabel("target")
+plt.title("Decision Tree Regression")
+plt.legend()
+plt.show()
+
+
+
+
+
